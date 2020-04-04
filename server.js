ce36b2e90cbfcab4d14bb348456b3186c64af3c7
Patrick Rottlaender <p.rottlaender@icloud.com>
2020-04-04 08:24:47 +0200

    initial commit package.json and server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+
+// load http module
+const http = require('http')
+
+
+// load the Express module
+const express = require('express')
+const app = express()
+
+// define the routes
+app.get('/', (req, res) => {
+  res.send('Hello, this is my first express home Page')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Hello, this is my first express about Page')
+})
+
+
+// create the server
+const server = http.createServer(app);
+
+// server listen for any incoming requests
+server.listen(3000);
+
+console.log('My express web server is alive and running at port 3000')
